b5ffb5a2cf6f9c895c8108eccfe6ecb445a9743b
David Osborne <davidmosborne@yahoo.com>
2016-04-08 19:46:23 +0100

    Removed uncessary code-behind.

--- web/MasterPage.master.cs
+++ web/MasterPage.master.cs
@@ -1,14 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-
-public partial class MasterPage : System.Web.UI.MasterPage
+﻿public partial class MasterPage : System.Web.UI.MasterPage
 {
-    protected void Page_Load(object sender, EventArgs e)
-    {
-
-    }
 }
